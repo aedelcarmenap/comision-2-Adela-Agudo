ccedc0e72cb6fdc9704246c7b38fdc1bb820c01f
adela <adeagu_ap@hotmail.com>
2023-11-30 15:31:28 -0300

    principal

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,22 +1,22 @@
-export const TOKEN_SECRET = 'some secret key';
-import dotenv from "dotenv"; 
+export const TOKEN_SECRET = "some secret key";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-//puerto 
+//puerto
 export const settingDotEnvPort = () => {
   return { port: process.env.PORT };
 };
-//base de datos 
+//base de datos
 export const settingDotEnvDb = () => {
   return {
     db: {
-      host: process.env.DB_HOST, localhost: process.env.DB_LOCALHOST,
+      host: process.env.DB_HOST,
+      localhost: process.env.DB_LOCALHOST,
     },
   };
 };
-//palabra secreta del token 
+//palabra secreta del token
 export const settingSecretToken = () => {
   return { secret: process.env.SECRET };
 };
-
